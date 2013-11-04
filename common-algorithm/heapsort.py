0eefd0daf85eea2f05cfc990abebec1c763489c7
ifkite <if.kite@gmail.com>
2013-11-04 19:24:50 +0800

    add None recursive version

--- common-algorithm/heapsort.py
+++ common-algorithm/heapsort.py
@@ -16,6 +16,25 @@ def max_heapify(heap,i,heap_len):
         heap[i],heap[largest]=heap[largest],heap[i]
         max_heapify(heap,largest,heap_len)
 
+'''
+None recursive version
+def max_heapify(heap,i,heap_len):
+    while True:
+        left=(i<<1)+1
+        right=(i<<1)+2
+        if left<=heap_len and heap[i]<heap[left]:
+            largest=left
+        else:
+            largest=i
+        if right<=heap_len and heap[largest]<heap[right]:
+            largest=right
+        if largest is not i:
+            heap[i],heap[largest]=heap[largest],heap[i]
+            i=largest
+        else:
+            break
+'''
+
 if __name__=='__main__':
     heap=[random.randint(1,10) for x in range(1,10)]
     #build heap
